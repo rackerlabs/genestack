abd3233b306e0e21d6d6be087d159c0ca6a948da
Luke Repko <luke.repko@rackspace.com>
2024-08-01 12:21:55 -0500

    fix: delint bootstrap script for ShellCheck

--- bootstrap.sh
+++ bootstrap.sh
@@ -16,8 +16,8 @@
 export LC_ALL=C.UTF-8
 mkdir -p ~/.venvs
 
-BASEDIR="$(dirname $0)"
-cd ${BASEDIR} || error "Could not change to ${BASEDIR}"
+BASEDIR="$(dirname "$0")"
+cd "${BASEDIR}" || error "Could not change to ${BASEDIR}"
 
 source scripts/lib/functions.sh
 
@@ -26,12 +26,15 @@ if [[ -z "${GENESTACK_PRODUCT}" ]]; then
 fi
 
 # Which config to bootstrap
-test -f "${GENESTACK_CONFIG}/product" 2>/dev/null && export GENESTACK_PRODUCT=$(head -n1 ${GENESTACK_CONFIG}/product)
+if test -f "${GENESTACK_CONFIG}/product" 2>/dev/null; then
+  GENESTACK_PRODUCT=$(head -n1 "${GENESTACK_CONFIG}"/product)
+  export GENESTACK_PRODUCT
+fi
 
 set -e
 
 success "Environment variables:"
-env | egrep '^(SUDO|RPC_|ANSIBLE_|GENESTACK_|K8S|CONTAINER_|OPENSTACK_|OSH_)' | sort -u
+env | grep -E '^(SUDO|RPC_|ANSIBLE_|GENESTACK_|K8S|CONTAINER_|OPENSTACK_|OSH_)' | sort -u
 
 success "Installing base packages (git):"
 apt update
@@ -39,7 +42,7 @@ apt update
 DEBIAN_FRONTEND=noninteractive \
   apt-get -o "Dpkg::Options::=--force-confdef" \
           -o "Dpkg::Options::=--force-confold" \
-          -qy install make git python3-pip python3-venv jq make 2>&1 > ~/genestack-base-package-install.log
+          -qy install make git python3-pip python3-venv jq make > ~/genestack-base-package-install.log 2>&1
 
 
 if [ $? -gt 1 ]; then
@@ -50,35 +53,34 @@ fi
 
 # Install project dependencies
 success "Installing genestack dependencies"
-test -L $GENESTACK_CONFIG 2>&1 || mkdir -p ${GENESTACK_CONFIG}
+test -L "$GENESTACK_CONFIG" 2>&1 || mkdir -p "${GENESTACK_CONFIG}"
 
 # Set config
-test -f $GENESTACK_CONFIG/provider || echo ${K8S_PROVIDER} > ${GENESTACK_CONFIG}/provider
-test -f $GENESTACK_CONFIG/product || echo ${GENESTACK_PRODUCT} > ${GENESTACK_CONFIG}/product
-mkdir -p $GENESTACK_CONFIG/inventory/group_vars ${GENESTACK_CONFIG}/inventory/credentials
+test -f "$GENESTACK_CONFIG"/provider || echo "${K8S_PROVIDER}" > "${GENESTACK_CONFIG}"/provider
+test -f "$GENESTACK_CONFIG"/product || echo "${GENESTACK_PRODUCT}" > "${GENESTACK_CONFIG}"/product
+mkdir -p "$GENESTACK_CONFIG"/inventory/group_vars "${GENESTACK_CONFIG}"/inventory/credentials
 
 # Copy default k8s config
 test -d "ansible/inventory/${GENESTACK_PRODUCT}" || error "Product Config ${GENESTACK_PRODUCT} does not exist here"
 PRODUCT_DIR="ansible/inventory/${GENESTACK_PRODUCT}"
-if [ $(find ${GENESTACK_CONFIG}/inventory -name \*.yaml -o -name \*.yml 2>/dev/null |wc -l) -eq 0 ]; then
-  cp -r ${PRODUCT_DIR}/* ${GENESTACK_CONFIG}/inventory
+if [ "$(find "${GENESTACK_CONFIG}"/inventory -name \*.yaml -o -name \*.yml 2>/dev/null |wc -l)" -eq 0 ]; then
+  cp -r "${PRODUCT_DIR}"/* "${GENESTACK_CONFIG}"/inventory
 fi
 
 # Copy gateway-api exmaple configs
 test -d "$GENESTACK_CONFIG"/gateway-api || cp -a "${BASEDIR}"/etc/gateway-api "$GENESTACK_CONFIG"/
 
-# Prepare Ansible
+# Create venv and prepare Ansible
 python3 -m venv ~/.venvs/genestack
 ~/.venvs/genestack/bin/pip install pip --upgrade
+# shellcheck disable=SC1090
 source ~/.venvs/genestack/bin/activate && success "Switched to venv ~/.venvs/genestack"
-
-pip install -r ${BASEDIR}/requirements.txt && success "Installed ansible package"
-
-ansible-playbook scripts/get-ansible-collection-requirements.yml \
+pip install -r "${BASEDIR}"/requirements.txt && success "Installed ansible package"
+ansible-playbook "${BASEDIR}"/scripts/get-ansible-collection-requirements.yml \
   -e collections_file="${ANSIBLE_COLLECTION_FILE}" \
   -e user_collections_file="${USER_COLLECTION_FILE}"
 
-source  ${BASEDIR}/scripts/genestack.rc
+source  "${BASEDIR}"/scripts/genestack.rc
 success "Environment sourced per ${BASEDIR}/scripts/genestack.rc"
 
 message "OpenStack Release: ${OPENSTACK_RELEASE}"
