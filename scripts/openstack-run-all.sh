3bcec2bfe18d24b65290ccd9354a9279ef286fc3
Tim Olow <tim@eth0.com>
2024-10-17 14:07:54 -0500

    Add barbican install to openstack-run-all.sh
    Adding Barbican support into script openstack-run-all.sh

--- scripts/openstack-run-all.sh
+++ scripts/openstack-run-all.sh
@@ -110,6 +110,20 @@ helm upgrade --install placement ./placement --namespace=openstack \
     --post-renderer /opt/genestack/base-kustomize/kustomize.sh \
     --post-renderer-args placement/base &
 
+helm upgrade --install barbican ./barbican \
+    --namespace=openstack \
+    --wait \
+    --timeout 120m \
+    -f /etc/genestack/helm-configs/barbican/barbican-helm-overrides.yaml \
+    --set endpoints.identity.auth.admin.password="$(kubectl --namespace openstack get secret keystone-admin -o jsonpath='{.data.password}' | base64 -d)" \
+    --set endpoints.identity.auth.barbican.password="$(kubectl --namespace openstack get secret barbican-admin -o jsonpath='{.data.password}' | base64 -d)" \
+    --set endpoints.oslo_db.auth.admin.password="$(kubectl --namespace openstack get secret mariadb -o jsonpath='{.data.root-password}' | base64 -d)" \
+    --set endpoints.oslo_db.auth.barbican.password="$(kubectl --namespace openstack get secret barbican-db-password -o jsonpath='{.data.password}' | base64 -d)" \
+    --set endpoints.oslo_messaging.auth.admin.password="$(kubectl --namespace openstack get secret rabbitmq-default-user -o jsonpath='{.data.password}' | base64 -d)" \
+    --set endpoints.oslo_messaging.auth.barbican.password="$(kubectl --namespace openstack get secret barbican-rabbitmq-password -o jsonpath='{.data.password}' | base64 -d)" \
+    --post-renderer /etc/genestack/kustomize/kustomize.sh \
+    --post-renderer-args barbican/base &
+
 helm upgrade --install octavia ./octavia \
     --namespace=openstack \
     --wait \
