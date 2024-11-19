37b5614a9cf88b75ba0cc1228b21a3bf852e8be9
Jake Briggs <briggs679@gmail.com>
2024-11-19 14:09:22 -0600

    Updated workflows
    Corrected workflow for libvirt and updated install script

--- bin/install-libvirt.sh
+++ bin/install-libvirt.sh
@@ -8,16 +8,22 @@ pushd /opt/genestack/submodules/openstack-helm-infra || exit
 # Base helm upgrade command
 HELM_CMD="helm upgrade --install libvirt ./libvirt \
     --namespace=openstack \
-    --timeout 120m \
-    -f /opt/genestack/base-helm-configs/libvirt/libvirt-helm-overrides.yaml"
+    --timeout 120m"
+
+# Add the base overrides file
+HELM_CMD+=" -f /opt/genestack/base-helm-configs/libvirt/libvirt-helm-overrides.yaml"
 
 # Check if YAML files exist in the specified directory
 if compgen -G "${CONFIG_DIR}/*.yaml" > /dev/null; then
-    # Add all YAML files from the directory to the helm command
+    # Append all YAML files from the directory to the helm command
     for yaml_file in "${CONFIG_DIR}"/*.yaml; do
         HELM_CMD+=" -f ${yaml_file}"
     done
 fi
+
+# Append the post-renderer and its arguments
+HELM_CMD+=" --post-renderer /etc/genestack/kustomize/kustomize.sh"
+HELM_CMD+=" --post-renderer-args barbican/base $@"
 
 # Run the helm command
 eval "${HELM_CMD}"
