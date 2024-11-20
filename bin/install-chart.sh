f8edd16ba9d5344c21c3e19ee1554509bd742f7b
Adam Fabian <adam.fabian@rackspace.com>
2024-11-20 14:38:01 -0600

    fix: remove gojq dependency for install-chart.sh

--- bin/install-chart.sh
+++ bin/install-chart.sh
@@ -1,63 +1,41 @@
 #!/bin/bash
 
-# Using a first arg of '-t' will just echo commands
-if [[ "$1" == "-t" ]]
-then
-    ECHOTEST="echo "
-    shift
-fi
 CHART="$1"
 shift # preserve "$@" for --post-renderer-args
+
 GENESTACK_DIR="${GENESTACK_DIR:-/opt/genestack}"
 CHART_META_FILE=\
 "${CHART_META_FILE:-$GENESTACK_DIR/bin/chart-install-meta.yaml}"
-
-if ! GOJQ="$(command -v gojq)"
-then
-    echo "Please install gojq for reading $CHART_META_FILE"
-    exit 1
-fi
-
-get_chart_info() {
-    local LIST=""
-    if [[ "$1" == "-l" ]]
-    then
-        local LIST=" | .[]"
-        shift
-    fi
-    local CHART="$1"
-    local PARAM="$2"
-    local result
-    result="$($GOJQ -r --yaml-input ".$CHART.$PARAM$LIST" "$CHART_META_FILE")"
-    if [[ "$result" == "null" ]]
-    then
-        echo "missing \"$CHART\" parameter \"$PARAM\"" > /dev/fd/2
-        exit 1
-    else
-        echo "$result"
-    fi
-}
-
-for var in repoName releaseName name url namespace
-do
-    var_name="$(echo "$var" | tr '[:lower:]' '[:upper:]')"
-    declare "$var_name=$(get_chart_info "$CHART" "$var")"
-done
-
 GENESTACK_DIR="${GENESTACK_DIR:-/opt/genestack}"
 GENESTACK_CONFIG_DIR="${GENESTACK_CONFIG_DIR:-/etc/genestack}"
-
 GENESTACK_CHART_DIR=\
 "${GENESTACK_CHART_DIR:-$GENESTACK_DIR/base-helm-configs/$CHART}"
 GENESTACK_CHART_CONFIG_DIR=\
 "${GENESTACK_CHART_CONFIG_DIR:-$GENESTACK_CONFIG_DIR/helm-configs/$CHART}"
+# This Python needs PyYAML, which the normal Genestack venv will have.
+YAML_PARSER_PYTHON="${YAML_PARSER_PYTHON:-$(which python)}"
+YAML_PARSER_PY="${YAML_PARSER_PY:-$GENESTACK_DIR/bin/yamlparse.py}"
+YAML_PARSER_CMD="${YAML_PARSER_CMD:-$YAML_PARSER_PYTHON $YAML_PARSER_PY}"
+
+IFS= readarray -t chart_values < <($YAML_PARSER_CMD "$CHART_META_FILE" "$CHART")
+
+values_length=${#chart_values[@]}
+
+# This for loop takes values from the YAML file `namespace: prometheus` and
+# sets variables like NAMESPACE=prometheus for use by the script.
+for (( i=0; i<values_length; i+=2 ))
+do
+    var_name="$(echo "${chart_values[i]}" | tr '[:lower:]' '[:upper:]')"
+    declare "$var_name=${chart_values[i+1]}"
+done
 
 # Though it probably wouldn't make any difference for all of the
 # $GENESTACK_CONFIG_DIR files to come last, this takes care to fully preserve
 # the order
 echo "Including overrides in order:"
 values_args=()
-for BASE_FILENAME in $(get_chart_info -l "$CHART" valuesFiles)
+set -o noglob # Prevent glob expansions in $VALUESFILES
+for BASE_FILENAME in $VALUESFILES
 do
     for DIR in "$GENESTACK_CHART_DIR" "$GENESTACK_CHART_CONFIG_DIR"
     do
@@ -71,9 +49,21 @@ do
 done
 echo
 
-$ECHOTEST helm repo add "$REPONAME" "$URL"
-$ECHOTEST helm repo update
-$ECHOTEST helm upgrade --install "$RELEASENAME" "$REPONAME/$NAME" \
+# Run script as ECHO_TEST=true install-chart.sh to see the commands that would
+# run formatted as a Python list to clearly distinguish whitespace from separate
+# arguments
+run_or_test_print () {
+    if [[ "$ECHO_TEST" == "true" ]]
+    then
+        python -c 'import sys; print(sys.argv[1:])' "$@"
+    else
+        "$@"
+    fi
+}
+
+run_or_test_print helm repo add "$REPONAME" "$URL"
+run_or_test_print helm repo update
+run_or_test_print helm upgrade --install "$RELEASENAME" "$REPONAME/$NAME" \
     --create-namespace --namespace="$NAMESPACE" \
     --timeout 10m \
     "${values_args[@]}" \
---
+++ bin/yamlparse.py
@@ -0,0 +1,26 @@
+# This works with install-chart.sh to get the information to run the script.
+# It doesn't work very well as any kind of stand-alone utility.
+
+import sys
+import yaml
+
+if len(sys.argv) < 3:
+    print(f"Usage: {sys.argv[0]} <filename> <chart>")
+    sys.exit(1)
+
+filename = sys.argv[1]
+chart = sys.argv[2]
+
+with open(filename, "r") as conf_file:
+    conf_yaml = yaml.safe_load(conf_file)
+
+if chart not in conf_yaml:
+    print(f"No chart '{chart}' in file {filename}")
+    sys.exit(1)
+
+for key, value in conf_yaml[chart].items():
+    print(f"{key}")
+    if not isinstance(value, list):
+        print(f"{value}")
+    else:
+        print(*value)
