957a112069c91a699cdbfba968b0e2567b79cf42
Kevin Carter <kevin.carter@rackspace.com>
2024-11-20 15:40:25 -0600

    feat: create a single images overlay
    With this change all of the images that we use across our charts will now be defined
    in a single place. This will better allow an automated pipeline to control our image
    usage and ensure that we're maintaining a unified set of images for a given release,
    branch, or deployment.
    
    * All images are using the builtin kustomize image replacement solution.
    * The default base image set is now defined as 2024.1
    * The helm values are defining the `image-<name>` string within them, permitting
      the override to take place
    * The kustomization files have been updated to all use the updated resources
      defined in the image overlay
    
    Signed-off-by: Kevin Carter <kevin.carter@rackspace.com>

--- base-kustomize/kustomize.sh
+++ base-kustomize/kustomize.sh
@@ -1,7 +1,17 @@
 #!/usr/bin/env bash
 set -e
-KUSTOMIZE_DIR=${1:-$GENESTACK_KUSTOMIZE_ARG}
+
+clean_up() {
+  test -d "$tmp_dir" && rm -rf "$tmp_dir"
+}
+
+tmp_dir=$(mktemp -d -t kustomize-XXXXXXXXXX)
+trap "clean_up $tmp_dir" EXIT
+
+KUSTOMIZE_DIR="${1:-$GENESTACK_KUSTOMIZE_ARG}"
 pushd "$(dirname "${BASH_SOURCE[0]}")" &>/dev/null
     cat <&0 > "${KUSTOMIZE_DIR}"/../base/all.yaml
-    kubectl kustomize --reorder='none' "${KUSTOMIZE_DIR}"
+    cp -R "${KUSTOMIZE_DIR}"/../../base-images/"${2:-latest}"/* "${tmp_dir}"/
+    kubectl kustomize "${KUSTOMIZE_DIR}" > "${tmp_dir}"/compiled.yaml
+    kubectl kustomize "${tmp_dir}" | tee "${KUSTOMIZE_DIR}"/../base/.compiled-output  # for debugging
 popd &>/dev/null
