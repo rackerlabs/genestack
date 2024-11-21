9a2ea26c57432f24befe302bc207a106f57f65a9
Jake Briggs <briggs679@gmail.com>
2024-11-21 08:36:04 -0600

    Memcached convert to helm (#583)
    * Clean branch
    
    Thisi s the changes for memcached to convert to using helm for the install process
    
    * Delete kustomize-memcached.yaml

---
+++ bin/install-memcached.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+# Default parameter value
+TARGET=${1:-base}
+
+# Directory to check for YAML files
+CONFIG_DIR="/etc/genestack/helm-configs/memcached"
+
+# Helm command setup
+HELM_CMD="helm upgrade --install memcached oci://registry-1.docker.io/bitnamicharts/memcached \
+    --namespace=openstack \
+    --timeout 120m \
+    --post-renderer /etc/genestack/kustomize/kustomize.sh \
+    --post-renderer-args memcached/${TARGET} \
+    -f /opt/genestack/base-helm-configs/memcached/memcached-helm-overrides.yaml"
+
+# Check if YAML files exist in the specified directory
+if compgen -G "${CONFIG_DIR}/*.yaml" > /dev/null; then
+    # Add all YAML files from the directory to the helm command
+    for yaml_file in "${CONFIG_DIR}"/*.yaml; do
+        HELM_CMD+=" -f ${yaml_file}"
+    done
+fi
+
+# Run the helm command
+echo "Executing Helm command:"
+echo "${HELM_CMD}"
+eval "${HELM_CMD}"
