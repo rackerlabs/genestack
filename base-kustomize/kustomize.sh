fac3138b750e84b988a76e922fb9e9f6bfa26e65
Kevin Carter <kevin@cloudnull.com>
2024-11-21 15:16:08 -0600

    fix: resolve Kustomize deprecations (#584)
    Kustomize has deprecated the use of a couple feature flags and we need to
    make adjustments to ensure that we're maintaining the environment in
    expected ways.
    
    * remove the CLI option from `kustomize.sh` to disable automatic reordering
    * add the `sortOptions` keyword to the base kustomization files to ensure
      resource sorting is handled as fifo.
    * replace the `bases` keyword with the `resources` keyword
    
    Signed-off-by: Kevin Carter <kevin.carter@rackspace.com>

--- base-kustomize/kustomize.sh
+++ base-kustomize/kustomize.sh
@@ -3,5 +3,5 @@ set -e
 KUSTOMIZE_DIR=${1:-$GENESTACK_KUSTOMIZE_ARG}
 pushd "$(dirname "${BASH_SOURCE[0]}")" &>/dev/null
     cat <&0 > "${KUSTOMIZE_DIR}"/../base/all.yaml
-    kubectl kustomize --reorder='none' "${KUSTOMIZE_DIR}"
+    kubectl kustomize "${KUSTOMIZE_DIR}"
 popd &>/dev/null
