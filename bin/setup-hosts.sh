02d46936d40cea2f44cadde5588d2553fe9fe825
aedan <briggs679@gmail.com>
2025-02-02 10:34:15 -0600

    Updated install scripts
    Updated install scripts to accept a global override as well as service override and corrected bootstrap to setup the global overrides directory as well as the service overrides directorys

--- bin/setup-hosts.sh
+++ bin/setup-hosts.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 source /opt/genestack/scripts/genestack.rc
 cd /opt/genestack/ansible/playbooks
-ansible-playbook /root/genestack-scripts/prep-nodes.yaml
-# Start kube install
 ansible-playbook host-setup.yml
 cd /opt/genestack/submodules/kubespray
 ansible-playbook cluster.yml
