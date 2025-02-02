a12a1dd5c609ad6e70ee8603eff347240242ce42
aedan <briggs679@gmail.com>
2025-02-02 16:16:13 -0600

    Update setup-hosts.sh

--- bin/setup-hosts.sh
+++ bin/setup-hosts.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 source /opt/genestack/scripts/genestack.rc
-cd /opt/genestack/ansible/playbooks
+cd /opt/genestack/ansible/playbooks || exit 1
 ansible-playbook host-setup.yml
-cd /opt/genestack/submodules/kubespray
+cd /opt/genestack/submodules/kubespray || exit 1
 ansible-playbook cluster.yml
