4fee828115eda236aaca62257a8fe7182f4095ff
Kevin Carter <kevin@cloudnull.com>
2025-02-24 17:58:31 -0600

    chore(libvirt): convert libvirt helm chart from submodule to repo (#807)
    This change removes the need to carry the openstack-helm-infra chart
    for the purposes of providing a libvirt deployment. The base helm
    file has been updated and simplified, reducing the values we carry
    to only what we need.
    
    Related Issue: https://github.com/rackerlabs/genestack/issues/809
    Signed-off-by: Kevin Carter <kevin.carter@rackspace.com>

--- bin/install-libvirt.sh
+++ bin/install-libvirt.sh
@@ -5,9 +5,7 @@ GLOBAL_OVERRIDES_DIR="/etc/genestack/helm-configs/global_overrides"
 SERVICE_CONFIG_DIR="/etc/genestack/helm-configs/libvirt"
 BASE_OVERRIDES="/opt/genestack/base-helm-configs/libvirt/libvirt-helm-overrides.yaml"
 
-pushd /opt/genestack/submodules/openstack-helm-infra || exit 1
-
-HELM_CMD="helm upgrade --install libvirt ./libvirt \
+HELM_CMD="helm upgrade --install libvirt openstack-helm-infra/libvirt --version 2024.2.92+628a320c \
     --namespace=openstack \
     --timeout 120m"
 
@@ -26,8 +24,9 @@ done
 
 HELM_CMD+=" $@"
 
+helm repo add openstack-helm-infra https://tarballs.opendev.org/openstack/openstack-helm-infra
+helm repo update
+
 echo "Executing Helm command:"
 echo "${HELM_CMD}"
 eval "${HELM_CMD}"
-
-popd || exit 1
