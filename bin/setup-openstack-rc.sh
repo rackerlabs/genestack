9753ae49dcb7a6898df3c9e9ee20c98d5a909f98
Kevin Carter <kevin@cloudnull.com>
2025-03-11 14:17:44 -0500

    feat: add / update scripts for install (#888)
    This change updates the lab and install scripts to use the
    hyperconverged lab process. The setup scripts will now run
    in parallel where it can.
    
    A new script was created to generate the openstack rc "clouds"
    file.
    
    Signed-off-by: Kevin Carter <kevin.carter@rackspace.com>

---
+++ bin/setup-openstack-rc.sh
@@ -0,0 +1,48 @@
+#!/usr/local/env bash
+set -e
+
+function installYq() {
+    export VERSION=v4.2.0
+    export BINARY=yq_linux_amd64
+    wget https://github.com/mikefarah/yq/releases/download/${VERSION}/${BINARY}.tar.gz -O - | tar xz && mv ${BINARY} /usr/local/bin/yq
+}
+
+yq --version || (echo "yq is not installed. Attempting to install yq" && installYq)
+
+USER_NAME="$(who am i | awk '{print $1}')"
+USER_PATH="$(getent passwd ${USER_NAME} | awk -F':' '{print $6}')"
+CONFIG_PATH="${USER_PATH}/.config/openstack"
+CONFIG_FILE="${CONFIG_PATH}/genestack-clouds.yaml"
+
+mkdir -p "${CONFIG_PATH}"
+
+cat > "${CONFIG_FILE}" <<EOF
+cache:
+  auth: true
+  expiration_time: 3600
+clouds:
+  default:
+    auth:
+      auth_url: $(kubectl --namespace openstack get secret keystone-keystone-admin -o jsonpath='{.data.OS_AUTH_URL}' | base64 -d)
+      project_name: $(kubectl --namespace openstack get secret keystone-keystone-admin -o jsonpath='{.data.OS_PROJECT_NAME}' | base64 -d)
+      tenant_name: $(kubectl --namespace openstack get secret keystone-keystone-admin -o jsonpath='{.data.OS_USER_DOMAIN_NAME}' | base64 -d)
+      project_domain_name: $(kubectl --namespace openstack get secret keystone-keystone-admin -o jsonpath='{.data.OS_PROJECT_DOMAIN_NAME}' | base64 -d)
+      username: $(kubectl --namespace openstack get secret keystone-keystone-admin -o jsonpath='{.data.OS_USERNAME}' | base64 -d)
+      password: $(kubectl --namespace openstack get secret keystone-keystone-admin -o jsonpath='{.data.OS_PASSWORD}' | base64 -d)
+      user_domain_name: $(kubectl --namespace openstack get secret keystone-keystone-admin -o jsonpath='{.data.OS_USER_DOMAIN_NAME}' | base64 -d)
+    region_name: $(kubectl --namespace openstack get secret keystone-keystone-admin -o jsonpath='{.data.OS_REGION_NAME}' | base64 -d)
+    interface: $(kubectl --namespace openstack get secret keystone-keystone-admin -o jsonpath='{.data.OS_INTERFACE}' | base64 -d)
+    identity_api_version: "3"
+EOF
+
+if [ -f "${CONFIG_PATH}/clouds.yaml" ]; then
+    /usr/local/bin/yq eval-all 'select(filename == "'"${CONFIG_PATH}/clouds.yaml"'") * select(filename == "'"${CONFIG_FILE}"'")' \
+    "${CONFIG_FILE}" \
+    "${CONFIG_PATH}/clouds.yaml" | tee "${CONFIG_PATH}/clouds.yaml.tmp"
+    mv "${CONFIG_PATH}/clouds.yaml.tmp" "${CONFIG_PATH}/clouds.yaml"
+    rm "${CONFIG_FILE}"
+else
+    mv "${CONFIG_FILE}" "${CONFIG_PATH}/clouds.yaml"
+fi
+
+chown -R "${USER_NAME}:${USER_NAME}" "${CONFIG_PATH}"
--- bin/setup-openstack.sh
+++ bin/setup-openstack.sh
@@ -1,39 +1,43 @@
-#!/bin/bash
-#Deploy Keystone
+#!/usr/local/env bash
+set -e
+
+export GENESTACK_INSTALL_TELEMETRY=${GENESTACK_INSTALL_TELEMETRY:-false}
+
+# Track the PIDs of the services deploying in parallel
+pids=()
+
+function runTrackErator() {
+    exec "${1}" &
+    pids+=($!)
+}
+
+function waitErator () {
+    for pid in ${pids[*]}; do
+        wait "${pid}"
+    done
+}
+
+# Block on Keystone
 /opt/genestack/bin/install-keystone.sh
 
-# Deploy Barbican
-/opt/genestack/bin/install-barbican.sh
+# Run the rest of the services in parallel
+runTrackErator /opt/genestack/bin/install-glance.sh
+runTrackErator /opt/genestack/bin/install-heat.sh
+runTrackErator /opt/genestack/bin/install-barbican.sh
+runTrackErator /opt/genestack/bin/install-cinder.sh
+runTrackErator /opt/genestack/bin/install-placement.sh
+runTrackErator /opt/genestack/bin/install-nova.sh
+runTrackErator /opt/genestack/bin/install-neutron.sh
+runTrackErator /opt/genestack/bin/install-magnum.sh
+runTrackErator /opt/genestack/bin/install-octavia.sh
 
-# Deploy Glance
-/opt/genestack/bin/install-glance.sh
+# Install telemetry services
+if [ "${GENESTACK_INSTALL_TELEMETRY}" = true ]; then
+    runTrackErator /opt/genestack/bin/install-ceilometer.sh
+    runTrackErator /opt/genestack/bin/install-gnocchi.sh
+fi
 
-# Deploy Heat
-/opt/genestack/bin/install-heat.sh
+waitErator
 
-# Deploy Magnum
-/opt/genestack/bin/install-magnum.sh
-
-# Deploy Cinder
-/opt/genestack/bin/install-cinder.sh
-
-# Deploy placement
-/opt/genestack/bin/install-placement.sh
-
-# Deploy Nova
-/opt/genestack/bin/install-nova.sh
-
-# Deploy Neutron
-/opt/genestack/bin/install-neutron.sh
-
-# Deploy Octavia
-/opt/genestack/bin/install-octavia.sh
-
-# Deploy SkyLine
+# Install skyline after all services are up
 /opt/genestack/bin/install-skyline.sh
-
-# Deploy Ceilometer
-/opt/genestack/bin/install-ceilometer.sh
-
-# Deploy Gnocchi
-/opt/genestack/bin/install-gnocchi.sh
