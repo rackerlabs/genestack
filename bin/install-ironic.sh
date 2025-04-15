8a13f0e873f2e93151f878c702ff41bbed7509f7
Joe <109156995+joew7456@users.noreply.github.com>
2025-04-15 09:28:13 -0500

    first ironic push (#955)
    * first ironic push
    
    * fixing lint errors
    
    * fixing merge conlict and yaml lint errors
    
    * Fix indentation and comment spacing for linter compliance
    
    * Fix YAML indentation for linter compliance
    
    * Fix: correct 4-space indentation on line 11 for linter compliance
    
    * Fix: correct 4-space indentation on line 11 for linter compliance
    
    * Update ironic-rabbitmq-queue.yaml
    
    * Update ironic-rabbitmq-queue.yaml
    
    ---------
    
    Co-authored-by: Jake Briggs <briggs679@gmail.com>

---
+++ bin/install-ironic.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+GLOBAL_OVERRIDES_DIR="/etc/genestack/helm-configs/global_overrides"
+SERVICE_CONFIG_DIR="/etc/genestack/helm-configs/ironic"
+BASE_OVERRIDES="/opt/genestack/base-helm-configs/ironic/ironic-helm-overrides.yaml"
+
+HELM_CMD="helm upgrade --install ironic openstack-helm/ironic --version 2024.2.4087+f37dd907f-a96a6b95 \
+    --namespace=openstack \
+    --timeout 120m"
+
+HELM_CMD+=" -f ${BASE_OVERRIDES}"
+
+for dir in "$GLOBAL_OVERRIDES_DIR" "$SERVICE_CONFIG_DIR"; do
+    if compgen -G "${dir}/*.yaml" > /dev/null; then
+        for yaml_file in "${dir}"/*.yaml; do
+            # Avoid re-adding the base override if it matches
+            if [ "${yaml_file}" != "${BASE_OVERRIDES}" ]; then
+                HELM_CMD+=" -f ${yaml_file}"
+            fi
+        done
+    fi
+done
+
+HELM_CMD+=" --set endpoints.identity.auth.admin.password=\"\$(kubectl --namespace openstack get secret keystone-admin -o jsonpath='{.data.password}' | base64 -d)\""
+HELM_CMD+=" --set endpoints.identity.auth.ironic.password=\"\$(kubectl --namespace openstack get secret ironic-admin -o jsonpath='{.data.password}' | base64 -d)\""
+HELM_CMD+=" --set endpoints.oslo_db.auth.admin.password=\"\$(kubectl --namespace openstack get secret mariadb -o jsonpath='{.data.root-password}' | base64 -d)\""
+HELM_CMD+=" --set endpoints.oslo_db.auth.ironic.password=\"\$(kubectl --namespace openstack get secret ironic-db-password -o jsonpath='{.data.password}' | base64 -d)\""
+HELM_CMD+=" --set endpoints.oslo_cache.auth.memcache_secret_key=\"\$(kubectl --namespace openstack get secret os-memcached -o jsonpath='{.data.memcache_secret_key}' | base64 -d)\""
+HELM_CMD+=" --set conf.ironic.keystone_authtoken.memcache_secret_key=\"\$(kubectl --namespace openstack get secret os-memcached -o jsonpath='{.data.memcache_secret_key}' | base64 -d)\""
+HELM_CMD+=" --set conf.ironic.database.slave_connection=\"mysql+pymysql://ironic:\$(kubectl --namespace openstack get secret ironic-db-password -o jsonpath='{.data.password}' | base64 -d)@mariadb-cluster-secondary.openstack.svc.cluster.local:3306/ironic\""
+HELM_CMD+=" --set endpoints.oslo_messaging.auth.admin.password=\"\$(kubectl --namespace openstack get secret rabbitmq-default-user -o jsonpath='{.data.password}' | base64 -d)\""
+HELM_CMD+=" --set endpoints.oslo_messaging.auth.ironic.password=\"\$(kubectl --namespace openstack get secret ironic-rabbitmq-password -o jsonpath='{.data.password}' | base64 -d)\""
+
+HELM_CMD+=" --post-renderer /etc/genestack/kustomize/kustomize.sh"
+HELM_CMD+=" --post-renderer-args ironic/overlay"
+
+helm repo add openstack-helm https://tarballs.opendev.org/openstack/openstack-helm
+helm repo update
+
+HELM_CMD+=" $@"
+
+echo "Executing Helm command:"
+echo "${HELM_CMD}"
+eval "${HELM_CMD}"
