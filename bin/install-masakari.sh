259a427fc563383835111f8927c0c52c0865f33e
zhmarvi <zain.marvi@rackspace.com>
2025-06-03 16:32:09 +0000

    Fix helm overrides and chart version for masakari-helm

--- bin/install-masakari.sh
+++ bin/install-masakari.sh
@@ -4,7 +4,7 @@ GLOBAL_OVERRIDES_DIR="/etc/genestack/helm-configs/global_overrides"
 SERVICE_CONFIG_DIR="/etc/genestack/helm-configs/masakari"
 BASE_OVERRIDES="/opt/genestack/base-helm-configs/masakari/masakari-helm-overrides.yaml"
 
-HELM_CMD="helm upgrade --install masakari openstack-helm/masakari --version 2024.2.157+13651f45-628a320c  \
+HELM_CMD="helm upgrade --install masakari openstack-helm/masakari --version 2024.2.17+13651f45-628a320c  \
     --namespace=openstack \
     --timeout 10m"
 
