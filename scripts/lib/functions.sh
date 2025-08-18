308ad9fbfd45dd1f6acb59408101881f1e339f8b
Chris Breu <chris.breu@rackspace.com>
2025-08-18 09:49:28 -0500

    fix: add waiting for pkg mgr to be available

--- scripts/lib/functions.sh
+++ scripts/lib/functions.sh
@@ -24,8 +24,62 @@ sudo -l |grep -q NOPASSWD && SUDO_CMD="/usr/bin/sudo -n "
 test -f ~/.rackspace/datacenter && export RAX_DC="$(cat ~/.rackspace/datacenter |tr '[:upper:]' '[:lower:]')"
 test -f /etc/openstack_deploy/openstack_inventory.json && export RPC_CONFIG_IN_PLACE=true || export RPC_CONFIG_IN_PLACE=false
 
+# Global functions
 
- # Global functions
+wait_for_dnf_locks() {
+    local max_retries=180  # Maximum retries for dnf commands
+    local retry_delay=10 # Delay between retries in seconds
+
+    for i in $(seq 1 $max_retries); do
+        if sudo dnf clean all && sudo dnf makecache; then
+            echo "dnf locks released. Proceeding."
+            return 0 # Indicate success
+        else
+            echo "dnf still locked. Retrying in $retry_delay seconds..."
+            sleep $retry_delay
+        fi
+    done
+
+    echo "Error: Timed out after $max_retries retries waiting for dnf to become available." >&2
+    return 1 # Indicate failure
+}
+
+wait_for_apt_locks() {
+    local max_wait_time=180  # Maximum time to wait in seconds
+    local elapsed_time=0
+
+    while sudo fuser /var/lib/dpkg/lock-frontend >/dev/null 2>&1 || \
+          sudo fuser /var/lib/dpkg/lock >/dev/null 2>&1; do
+
+        if (( elapsed_time >= max_wait_time )); then
+            echo "Error: Timed out waiting for apt locks to release." >&2
+            return 1 # Indicate failure
+        fi
+
+        echo "Waiting for apt locks to release..."
+        sleep 5  # Wait for 5 seconds before checking again
+        elapsed_time=$((elapsed_time + 5))
+    done
+    echo "apt locks released. Proceeding."
+    return 0 # Indicate success
+}
+
+wait_for_package_manager_locks() {
+    # Check if apt or dnf is the package manager
+    if command -v apt-get &>/dev/null; then
+        echo "Detected apt package manager."
+        wait_for_apt_locks
+        return $?
+    elif command -v dnf &>/dev/null; then
+        echo "Detected dnf package manager."
+        wait_for_dnf_locks
+        return $?
+    else
+        echo "Error: Neither apt nor dnf package manager found." >&2
+        return 1
+    fi
+}
+
 function success {
   echo -e "\n\n\x1B[32m>> $1\x1B[39m"
 }
