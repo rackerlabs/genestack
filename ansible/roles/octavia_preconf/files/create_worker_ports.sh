f8cc1ec215e0d446fedf47d1e7e78673b8bc93f7
phillip.toohill <phillip.toohill@rackspace.com>
2025-08-27 11:52:33 -0500

    feat: Adding octavia worker port creation in octavia preconf (#1150)
    * feat: Adding octavia worker port creation in octavia preconf
    
    updated versions of octavia helm chart require ports created for the
    worker nodes as well as the health managers. I've simply duplicated the port
    creation as this may be something we yank out if/when octavia helm decides to make
    it optional.
    
    * fix port yaml naming
    
    * Add missing secgrp name
    
    (cherry picked from commit 4711953a8ffe3ba3503146e386ee1b4c6776698f)

---
+++ ansible/roles/octavia_preconf/files/create_worker_ports.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+# We need to create the ports with shell scripts
+# the ansible module currently doesn't provide
+# --host argument
+
+set -xe
+
+# Obtain the network_id and secgroup_id from and
+# cloud name from ansible task
+NET_ID=$1
+SECGRP_ID=$2
+CLOUD_NAME=$3
+
+export OS_CLOUD=$CLOUD_NAME
+
+# Obtain the list of kubernetes nodes with
+# "openstack-control-plane=enabled" label
+CONTROLLER_IP_PORT_LIST=()
+CTRLS=$(kubectl get nodes -l openstack-control-plane=enabled -o name | awk -F"/" '{print $2}')
+for node in $CTRLS
+do
+  node_short=$(echo "$node" | awk -F"." '{print $1}')
+  PORTNAME=octavia-worker-port-$node_short
+  if ! PORT_DATA=$(openstack port show "$PORTNAME" -c fixed_ips -f json); then
+    PORT_DATA=$(openstack port create "$PORTNAME" --security-group "$SECGRP_ID" \
+                                                  --device-owner Octavia:worker \
+                                                  --host="$node" \
+                                                  --network "$NET_ID" \
+                                                  -c fixed_ips \
+                                                  -f json)
+  fi
+  for IP in $(echo "$PORT_DATA" | awk 'BEGIN { FS = "\"" } /ip_address/ { print $(NF - 1) }'); do
+    CONTROLLER_IP_PORT_LIST+=("$IP:5555")
+  done
+done
+
+readarray -t sorted < <(for item in "${CONTROLLER_IP_PORT_LIST[@]}"; do echo "${item}"; done | sort)
+echo $(IFS=,; echo "${sorted[*]}") > /tmp/octavia_worker_controller_ip_port_list
