c701e598612100c8994541c24c3f6a615bd2bb76
Nitin Gupta <63388430+niti6869@users.noreply.github.com>
2025-09-17 11:05:55 +0530

    Removed service password

--- bin/create-secrets.sh
+++ bin/create-secrets.sh
@@ -103,7 +103,6 @@ blazar_rabbitmq_password=$(generate_password 64)
 blazar_db_password=$(generate_password 32)
 blazar_admin_password=$(generate_password 32)
 blazar_keystone_test_password=$(generate_password 32)
-blazar_keystone_service_password=$(generate_password 32)
 
 OUTPUT_FILE="/etc/genestack/kubesecrets.yaml"
 
@@ -732,15 +731,6 @@ metadata:
 type: Opaque
 data:
   password: $(echo -n $blazar_keystone_test_password | base64 -w0)
----
-apiVersion: v1
-kind: Secret
-metadata:
-  name: blazar-keystone-service-password
-  namespace: openstack
-type: Opaque
-data:
-  password: $(echo -n $blazar_keystone_service_password=$ | base64 -w0)
 EOF
 
 rm nova_ssh_key nova_ssh_key.pub
