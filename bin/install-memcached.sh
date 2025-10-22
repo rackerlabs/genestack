928e7954392e76dffd8ac4b9455faa20811a17c9
aedan <briggs679@gmail.com>
2025-10-22 09:05:55 -0500

    Fix: Reverted back to bitnami memcached and updated to 8.1.1

--- bin/install-memcached.sh
+++ bin/install-memcached.sh
@@ -19,7 +19,7 @@ if [ -z "$MEMCACHED_VERSION" ]; then
     exit 1
 fi
 
-HELM_CMD="helm upgrade --install memcached openstack-helm/memcached \
+HELM_CMD="helm upgrade --install memcached bitnami/memcached \
     --version ${MEMCACHED_VERSION} \
     --namespace=openstack \
     --timeout 120m \
@@ -41,7 +41,7 @@ done
 
 HELM_CMD+=" $@"
 
-helm repo add openstack-helm https://tarballs.opendev.org/openstack/openstack-helm/
+helm repo add bitnami https://charts.bitnami.com/bitnami
 helm repo update
 
 echo "Executing Helm command:"
