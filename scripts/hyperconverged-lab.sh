493f8209ec179b7eb059db335d358ab8e4e33e6b
Luke Repko <luke.repko@rackspace.com>
2025-12-09 15:49:00 -0600

    fix(setup): Improve yq installation and handling
    Add verbosity to the wget command so we can troubleshoot download
    failures if encountered more easily, as well as very clear and
    concise messaging around whether or not yq really got installed.
    
    YQ is more necessary on the remote host than the local host that runs
    the hyperconverged-lab script, so warn about it not getting installed
    locally but fail if not present on the remote host as it's a
    pre-requisite to running many install scripts.

--- scripts/hyperconverged-lab.sh
+++ scripts/hyperconverged-lab.sh
@@ -21,6 +21,201 @@
 
 set -o pipefail
 set -e
+SECONDS=0
+RUN_EXTRAS=0
+INCLUDE_LIST=()
+EXCLUDE_LIST=()
+
+export TEST_LEVEL="${TEST_LEVEL:-off}"
+
+# yq installation constants
+YQ_VERSION="v4.2.0"
+YQ_BINARY="yq_linux_amd64"
+
+function installYq() {
+    if wget https://github.com/mikefarah/yq/releases/download/${YQ_VERSION}/${YQ_BINARY}.tar.gz -O - | tar xz && sudo mv ${YQ_BINARY} /usr/local/bin/yq; then
+        echo "Successfully installed yq version ${YQ_VERSION}"
+        return 0
+    else
+        echo "Failed to install yq"
+        return 1
+    fi
+}
+
+# Install yq locally if needed...
+if ! yq --version 2> /dev/null; then
+  echo "yq is not installed. Attempting to install yq"
+  if ! installYq; then
+    echo "[WARNING] Failed to install yq locally"
+  fi
+fi
+
+
+# Default openstack components file
+# this controls which openstack service will be installed
+##...needed until default config is upstream...
+OS_CONFIG="
+components:
+  keystone: true
+  glance: true
+  heat: false
+  barbican: false
+  blazar: false
+  cloudkitty: false
+  cinder: true
+  freezer: false
+  placement: true
+  nova: true
+  neutron: true
+  magnum: false
+  octavia: false
+  masakari: false
+  manila: false
+  ceilometer: false
+  gnocchi: false
+  skyline: true
+  zaqar: false
+"
+echo -e "$OS_CONFIG" > $PWD/openstack-components.yaml
+
+while getopts "i:e:x" opt; do
+  case $opt in
+    x)
+      RUN_EXTRAS=1
+      ;;
+    i)
+      old_IFS="$IFS"
+      IFS=','
+      read -r -a INCLUDE_LIST <<< "$OPTARG"
+      IFS="$old_IFS"
+      ;;
+    e)
+      old_IFS="$IFS"
+      IFS=','
+      read -r -a EXCLUDE_LIST <<< "$OPTARG"
+      IFS="$old_IFS"
+      ;;
+    *)
+      echo "Usage: $0 [-i <list,of,services,to,include>]
+      [-e <ist,of,services,to,exclude>]
+      -x <flag only will run extra operations>\n"
+      echo "View the openstack-components.yaml for the services available to configure."
+      exit 1
+      ;;
+    \?) # Handle invalid options
+      echo "Invalid option: -$OPTARG" >&2
+      exit 1
+      ;;
+  esac
+done
+shift $((OPTIND-1))
+
+for option in "${INCLUDE_LIST[@]}"; do
+  yq -i ".components.$option = true" $PWD/openstack-components.yaml
+done
+for option in "${EXCLUDE_LIST[@]}"; do
+    yq -i ".components.$option = false" $PWD/openstack-components.yaml
+done
+
+if [ -z "${ACME_EMAIL}" ]; then
+  read -rp "Enter a valid email address for use with ACME, press enter to skip: " ACME_EMAIL
+fi
+
+# Use of ACME_EMAIL to default Email
+ACME_EMAIL="${ACME_EMAIL:-example@aol.com}"
+export ACME_EMAIL
+
+if [ -z "${GATEWAY_DOMAIN}" ]; then
+  echo "The domain name for the gateway is required, if you do not have a domain name press enter to use the default"
+  read -rp "Enter the domain name for the gateway [cluster.local]: " GATEWAY_DOMAIN
+  export GATEWAY_DOMAIN="${GATEWAY_DOMAIN:-cluster.local}"
+fi
+
+if [ -z "${OS_CLOUD}" ]; then
+  read -rp "Enter name of the cloud configuration used for this build [default]: " OS_CLOUD
+  export OS_CLOUD="${OS_CLOUD:-default}"
+fi
+
+if [ -z "${OS_FLAVOR}" ]; then
+  # List compatible flavors
+  FLAVORS=$(openstack flavor list --min-ram 16000 --min-disk 100 --sort-column Name -c Name -c RAM -c Disk -c VCPUs -f json)
+  DEFAULT_OS_FLAVOR=$(echo "${FLAVORS}" | jq -r '[.[] | select( all(.RAM; . < 24576) )] | .[0].Name')
+  echo "The following flavors are available for use with this build"
+  echo "${FLAVORS}" | jq -r '["Name", "RAM", "Disk", "VCPUs"], (.[] | [.Name, .RAM, .Disk, .VCPUs]) | @tsv' | column -t
+  read -rp "Enter name of the flavor to use for the instances [${DEFAULT_OS_FLAVOR}]: " OS_FLAVOR
+  export OS_FLAVOR=${OS_FLAVOR:-${DEFAULT_OS_FLAVOR}}
+fi
+
+# Set the default image and ssh username
+export OS_IMAGE="${OS_IMAGE:-Ubuntu 24.04}"
+if [ -z "${SSH_USERNAME}" ]; then
+  if ! IMAGE_DEFAULT_PROPERTY=$(openstack image show "${OS_IMAGE}" -f json -c properties); then
+    read -rp "Image not found. Enter the image name: " OS_IMAGE
+    IMAGE_DEFAULT_PROPERTY=$(openstack image show "${OS_IMAGE}" -f json -c properties)
+  fi
+  if [ "${IMAGE_DEFAULT_PROPERTY}" ]; then
+    if SSH_USERNAME=$(echo "${IMAGE_DEFAULT_PROPERTY}" | jq -r '.properties.default_user'); then
+      echo "Discovered the default username for the image ${OS_IMAGE} as ${SSH_USERNAME}"
+    fi
+  fi
+  if [ -z "${SSH_USERNAME}" ] || [ "${SSH_USERNAME}" = "null" ]; then
+    echo "The image ${OS_IMAGE} does not have a default user property, please enter the default username"
+    read -rp "Enter the default username for the image: " SSH_USERNAME
+  fi
+fi
+
+export LAB_NAME_PREFIX="${LAB_NAME_PREFIX:-hyperconverged}"
+
+export LAB_NETWORK_MTU="${LAB_NETWORK_MTU:-1500}"
+
+if ! openstack router show ${LAB_NAME_PREFIX}-router 2>/dev/null; then
+  openstack router create ${LAB_NAME_PREFIX}-router --external-gateway PUBLICNET
+fi
+
+if ! openstack network show ${LAB_NAME_PREFIX}-net 2>/dev/null; then
+  openstack network create ${LAB_NAME_PREFIX}-net \
+    --mtu ${LAB_NETWORK_MTU}
+fi
+
+if ! TENANT_SUB_NETWORK_ID=$(openstack subnet show ${LAB_NAME_PREFIX}-subnet -f json 2>/dev/null | jq -r '.id'); then
+  echo "Creating the ${LAB_NAME_PREFIX}-subnet"
+  TENANT_SUB_NETWORK_ID=$(
+    openstack subnet create ${LAB_NAME_PREFIX}-subnet \
+      --network ${LAB_NAME_PREFIX}-net \
+      --subnet-range 192.168.100.0/24 \
+      --dns-nameserver 1.1.1.1 \
+      --dns-nameserver 1.0.0.1 \
+      -f json | jq -r '.id'
+  )
+fi
+
+if ! openstack router show ${LAB_NAME_PREFIX}-router -f json 2>/dev/null | jq -r '.interfaces_info.[].subnet_id' | grep -q ${TENANT_SUB_NETWORK_ID}; then
+  openstack router add subnet ${LAB_NAME_PREFIX}-router ${LAB_NAME_PREFIX}-subnet
+fi
+
+if ! openstack network show ${LAB_NAME_PREFIX}-compute-net 2>/dev/null; then
+  openstack network create ${LAB_NAME_PREFIX}-compute-net \
+    --disable-port-security \
+    --mtu ${LAB_NETWORK_MTU}
+fi
+
+if ! TENANT_COMPUTE_SUB_NETWORK_ID=$(openstack subnet show ${LAB_NAME_PREFIX}-compute-subnet -f json 2>/dev/null | jq -r '.id'); then
+  echo "Creating the ${LAB_NAME_PREFIX}-compute-subnet"
+  TENANT_COMPUTE_SUB_NETWORK_ID=$(
+    openstack subnet create ${LAB_NAME_PREFIX}-compute-subnet \
+      --network ${LAB_NAME_PREFIX}-compute-net \
+      --subnet-range 192.168.102.0/24 \
+      --no-dhcp -f json | jq -r '.id'
+  )
+fi
+
+if ! openstack router show ${LAB_NAME_PREFIX}-router -f json | jq -r '.interfaces_info.[].subnet_id' | grep -q ${TENANT_COMPUTE_SUB_NETWORK_ID} 2>/dev/null; then
+  openstack router add subnet ${LAB_NAME_PREFIX}-router ${LAB_NAME_PREFIX}-compute-subnet
+fi
+
+if ! openstack security group show ${LAB_NAME_PREFIX}-http-secgroup 2>/dev/null; then
+  openstack security group create ${LAB_NAME_PREFIX}-http-secgroup
+fi
 
 SCRIPT_DIR=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &>/dev/null && pwd)
 
@@ -80,6 +275,42 @@ EXAMPLES:
 
     # Deploy Talos with specific services excluded
     $(basename "$0") talos -e skyline
+
+# Install yq on the remote host if not already present
+if ! command -v yq &> /dev/null; then
+  echo "Installing yq on remote host..."
+  if wget https://github.com/mikefarah/yq/releases/download/${YQ_VERSION}/${YQ_BINARY}.tar.gz -O - | tar xz && sudo mv ${YQ_BINARY} /usr/local/bin/yq; then
+    echo "Successfully installed yq version ${YQ_VERSION} on remote host"
+  else
+    echo "Failed to install yq on remote host"
+    exit 1
+  fi
+else
+  echo "yq already available on remote host: \$(yq --version)"
+fi
+
+# We need to clobber the sample or else we get a bogus LB vip
+cat > /etc/genestack/manifests/metallb/metallb-openstack-service-lb.yml <<EOF
+---
+apiVersion: metallb.io/v1beta1
+kind: IPAddressPool
+metadata:
+  name: gateway-api-external
+  namespace: metallb-system
+spec:
+  addresses:
+    - ${METAL_LB_IP}/32  # This is assumed to be the public LB vip address
+  autoAssign: false
+---
+apiVersion: metallb.io/v1beta1
+kind: L2Advertisement
+metadata:
+  name: openstack-external-advertisement
+  namespace: metallb-system
+spec:
+  ipAddressPools:
+    - gateway-api-external
+EOF
 
 UNINSTALL:
     Use the platform-specific uninstall scripts:
