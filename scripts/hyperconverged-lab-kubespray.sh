1dbfeb8cf6eddc234894b5e0ebc94066ea95df6c
Kevin Ayers <kevin.ayers@rackspace.com>
2026-06-08 14:21:48 -0500

    OSPC-2080 Ansiblize and/or Kustomize the setup for Trove

--- scripts/hyperconverged-lab-kubespray.sh
+++ scripts/hyperconverged-lab-kubespray.sh
@@ -937,7 +937,7 @@ if [ "${TEST_LEVEL}" = "off" ]; then
 
     # Trove Setup & Installation
     # Must be run after the flat network has been created
-    deployTrove "${SSH_USERNAME}" "${JUMP_HOST_VIP}" "${LAB_NAME_PREFIX}" "${COMPUTE_SUBNET_CIDR}" "${MGMT_SUBNET_CIDR}"
+    deployTrove "RegionOne" "flat" "false" "trove.cluster.local" "internal" "hyperconverged-key.pem"
 
 else
     # Wait for Nova and Neutron APIs to be ready before proceeding
